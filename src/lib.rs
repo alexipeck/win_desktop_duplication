25d2b2fd6e46aa96b5f7658659eab56bd70a599e
Alexi Peck <alexinicolaspeck@gmail.com>
2024-08-04 22:19:17 -0400

    Remove cross_platform_types feature instead using the windows feature for the inverse operation

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,25 @@
 #![doc = include_str ! ("../README.md")]
 
-#[cfg(not(feature = "cross_platform_types"))]
+#[cfg(feature = "windows")]
 pub mod devices;
-#[cfg(not(feature = "cross_platform_types"))]
+#[cfg(feature = "windows")]
 pub mod duplication;
-#[cfg(not(feature = "cross_platform_types"))]
+#[cfg(feature = "windows")]
 pub mod error;
-#[cfg(not(feature = "cross_platform_types"))]
+#[cfg(feature = "windows")]
 pub mod outputs;
-#[cfg(not(feature = "cross_platform_types"))]
+#[cfg(feature = "windows")]
 pub mod tex_reader;
-#[cfg(not(feature = "cross_platform_types"))]
+#[cfg(feature = "windows")]
 pub mod texture;
 pub mod types;
-#[cfg(not(feature = "cross_platform_types"))]
+#[cfg(feature = "windows")]
 mod utils;
 
-#[cfg(not(feature = "cross_platform_types"))]
+#[cfg(feature = "windows")]
 pub use duplication::*;
-#[cfg(not(feature = "cross_platform_types"))]
+#[cfg(feature = "windows")]
 pub use utils::{co_init, set_process_dpi_awareness};
 
-#[cfg(not(feature = "cross_platform_types"))]
+#[cfg(feature = "windows")]
 pub type Result<T> = core::result::Result<T, crate::error::DDApiError>;
